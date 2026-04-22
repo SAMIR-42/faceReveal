90fc082a23f43be563b511cecb74c771333da5f1
hey-Sam-u <samirpatil1626@gmail.com>
2026-04-22 11:37:01 +0530

    A

---
+++ pages/data.js
@@ -0,0 +1,34 @@
+export const categories = [
+    "overthinking",
+    "trust",
+    "emotional",
+    "ambition",
+    "social",
+    "introvert",
+    "confidence"
+  ];
+
+
+  export const data = {
+    overthinking: {
+      free: [
+        "You tend to overthink small situations.",
+        "You replay conversations in your mind."
+      ],
+      paid: [
+        "Your overthinking is linked to past situations where you felt misunderstood.",
+        "You often doubt your decisions even after making them."
+      ]
+    },
+  
+    trust: {
+      free: [
+        "You don’t trust people easily.",
+        "You observe people before opening up."
+      ],
+      paid: [
+        "Once trust is broken, you rarely give second chances.",
+        "You value loyalty more than anything."
+      ]
+    }
+  };
